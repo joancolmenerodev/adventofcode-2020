19768b5c1bde631f53afaf5aa9f06c6dd0fcec9f
Joan <joancolmenero@gmail.com>
2020-12-13 17:20:42 +0100

    Day1-Part2 solved

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,5 +1,21 @@
 class Day1 {
 
+    /*
+     the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+
+     Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+
+    For example, suppose your expense report contained the following:
+
+    1721
+    979
+    366
+    299
+    675
+    1456
+    In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+     */
+
     fun solveExpenses(list: List<Int>, goalSum: Int): Int {
 
         //Using the technique of hashing
@@ -11,10 +27,46 @@ class Day1 {
         }
         return -1
     }
+
+    fun solveThreeExpenses(list: List<Int>, goalSum: Int): Int {
+
+        //sort the list to use the two pointers technique (I'd used quicksort instead)
+        val sortedList = list.sorted()
+
+        var left: Int
+        var right: Int
+
+
+        for (index in 0..list.size - 2) {
+            left = index + 1
+            right = sortedList.size - 1
+
+            while (left < right) {
+                val candidate = sortedList[index] + sortedList[left] + sortedList[right]
+                when {
+                    //move left one position
+                    candidate < goalSum -> {
+                        left++
+                    }
+                    //move right one less position
+                    candidate > goalSum -> {
+                        right--
+                    }
+                    else -> {
+                        return sortedList[index] * sortedList[left] * sortedList[right]
+                    }
+                }
+            }
+        }
+        return -1
+
+    }
+
 }
 
-fun main(){
+fun main() {
     val solver = Day1()
     val input = loadResource("InputDay1.txt")
     println(solver.solveExpenses(input, 2020))
+    println(solver.solveThreeExpenses(input, 2020))
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -40,4 +40,20 @@ class Day1Test {
 
     }
 
+    @Test
+    @DisplayName(
+        """
+        Given a list of integers
+        When it checks if there's a sum between two numbers that is 2020
+        Then it adds all the numbers
+    """
+    )
+    fun addsAllTheNumbers() {
+        val testInput = listOf(1721, 979, 366, 299, 675, 1456)
+        val solver = Day1()
+        val actualResult = solver.solveThreeExpenses(testInput, GOAL_SUM)
+        actualResult shouldBe 241861950
+
+    }
+
 }
